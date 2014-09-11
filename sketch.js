b1630830d72e78b40df09df92e9f6ebc390e4f97
leeyeji <miracle458393@gmail.com>
2014-09-11 13:50:37 +0900

    add.0911 1
    1

--- sketch.js
+++ sketch.js
@@ -1,9 +1,14 @@
 function setup() {
   createCanvas(640, 480);
-  background(0);
+  
+  //(0); black, 255: white
+  // background(red, green, blue);
+  background(201. 241, 43);
+  
   stroke(255);
   noFill();
-  line(10, 10, 90, 10);
-    rect(20, 20, 80, 50);
-    ellipse(50, 50, 50, 50);
+
+  line(10, 10, 90, 10); // x1, y1, x2, y2
+  rect(20, 20, 80, 50); // x, y, width, height
+  ellipse(50, 50, 50, 50); // centerX, centerY, width, height
 }
